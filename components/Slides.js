0dd3000b561f539548b8aae690eaedd946b0520f
andrwsalcdo <asalcedo07@gmail.com>
2018-05-22 21:17:24 +0200

    add a renderProgressDots() to <Slides />. render same number of dots & slides

--- components/Slides.js
+++ components/Slides.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import { View, ScrollView, Dimensions } from 'react-native';
+import { View, ScrollView, Dimensions, Animated } from 'react-native';
 import { Button, Text } from 'native-base';
 
 const SCREEN_WIDTH = Dimensions.get('window').width;
 
-class Slides extends Component {
+class Slides extends React.Component {
   renderLastSlide(index) {
     if (index === this.props.data.length - 1) {
       return (
@@ -30,16 +30,38 @@ class Slides extends Component {
     ));
   }
 
+  renderProgressDots() {
+    return this.props.data.map((_, i) => {
+      return <Animated.View key={i} style={[styles.dots]} />;
+    });
+  }
+
   render() {
     return (
-      <ScrollView horizontal pagingEnabled>
-        {this.renderSlides()}
-      </ScrollView>
+      <View style={styles.container}>
+        <View style={{ width: SCREEN_WIDTH, height: SCREEN_WIDTH }}>
+          <ScrollView
+            horizontal
+            pagingEnabled
+            showsHorizontalScrollIndicator={false}
+          >
+            {this.renderSlides()}
+          </ScrollView>
+        </View>
+        <View style={{ flexDirection: 'row' }}>
+          {this.renderProgressDots()}
+        </View>
+      </View>
     );
   }
 }
 
 const styles = {
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
   button: {
     alignSelf: 'center',
     marginTop: 15
@@ -53,6 +75,13 @@ const styles = {
   slideText: {
     fontSize: 30,
     textAlign: 'center'
+  },
+  dots: {
+    height: 10,
+    width: 10,
+    backgroundColor: '#595959',
+    margin: 8,
+    borderRadius: 5
   }
 };
 
