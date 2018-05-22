f809d5d1142f821daead18d7ae20674acb4ae292
andrwsalcdo <asalcedo07@gmail.com>
2018-05-22 16:47:57 +0200

    add data for slides, & display <Slides /> in WelcomeScreen

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import Slides from '../components/Slides';
+
+const SLIDE_DATA = [
+  {
+    text: 'Welcome to JobApp'
+  },
+  {
+    text: 'Use this to find the job you really want'
+  },
+  { text: 'Set your location, and swipe away' }
+];
 
 class WelcomeScreen extends Component {
   render() {
     return (
-      <View>
-        <Text>This is the WelcomeScreen</Text>
-        <Text>This is the WelcomeScreen</Text>
-        <Text>This is the WelcomeScreen</Text>
-        <Text>This is the WelcomeScreen</Text>
-        <Text>This is the WelcomeScreen</Text>
-        <Text>This is the WelcomeScreen</Text>
+      <View style={{ flex: 1 }}>
+        <Slides data={SLIDE_DATA} />
       </View>
     );
   }
