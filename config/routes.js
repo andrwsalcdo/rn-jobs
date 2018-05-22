600df1fe83c3a3116c50e440791a36ca1a19467f
andrwsalcdo <asalcedo07@gmail.com>
2018-05-22 13:20:18 +0200

    create simple routes config. needs updating to hide tabs

---
+++ config/routes.js
@@ -0,0 +1,28 @@
+import {
+  createBottomTabNavigator,
+  createStackNavigator
+} from 'react-navigation';
+
+import AuthScreen from '../screens/AuthScreen';
+import WelcomeScreen from '../screens/WelcomeScreen';
+import MapScreen from '../screens/MapScreen';
+import DeckScreen from '../screens/DeckScreen';
+import SettingsScreen from '../screens/SettingsScreen';
+import ReviewScreen from '../screens/ReviewScreen';
+
+export const MainNavigator = createBottomTabNavigator({
+  welcome: WelcomeScreen,
+  auth: AuthScreen,
+  main: {
+    screen: createBottomTabNavigator({
+      map: MapScreen,
+      deck: DeckScreen,
+      review: {
+        screen: createStackNavigator({
+          review: ReviewScreen,
+          settings: SettingsScreen
+        })
+      }
+    })
+  }
+});
