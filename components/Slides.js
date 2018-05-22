21e6b55f0e28eac3894837848024b9c87d2cde63
andrwsalcdo <asalcedo07@gmail.com>
2018-05-22 17:22:44 +0200

    on <Slides /> render a button on the last slide with a onComplete callback

--- components/Slides.js
+++ components/Slides.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
-import { View, Text, ScrollView, Dimensions } from 'react-native';
+import { View, ScrollView, Dimensions } from 'react-native';
+import { Button, Text } from 'native-base';
 
 const SCREEN_WIDTH = Dimensions.get('window').width;
 
 class Slides extends Component {
+  renderLastSlide(index) {
+    if (index === this.props.data.length - 1) {
+      return (
+        <Button
+          rounded
+          success
+          large
+          style={styles.button}
+          onPress={this.props.onComplete}
+        >
+          <Text>Onwards!</Text>
+        </Button>
+      );
+    }
+  }
+
   renderSlides() {
-    return this.props.data.map(slide => (
+    return this.props.data.map((slide, idx) => (
       <View key={slide.text} style={styles.slide}>
         <Text style={styles.slideText}>{slide.text}</Text>
+        {this.renderLastSlide(idx)}
       </View>
     ));
   }
@@ -22,6 +40,10 @@ class Slides extends Component {
 }
 
 const styles = {
+  button: {
+    alignSelf: 'center',
+    marginTop: 15
+  },
   slide: {
     flex: 1,
     justifyContent: 'center',
@@ -29,7 +51,8 @@ const styles = {
     width: SCREEN_WIDTH
   },
   slideText: {
-    fontSize: 30
+    fontSize: 30,
+    textAlign: 'center'
   }
 };
 
