05ae62c95aa8c78dac0e1243c669185e82935bf2
andrwsalcdo <asalcedo07@gmail.com>
2018-05-22 13:18:04 +0200

    add static navigationOptions to ReviewScreen. button to SettingsScreen

--- screens/ReviewScreen.js
+++ screens/ReviewScreen.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 
 class ReviewScreen extends Component {
+  static navigationOptions = ({ navigation }) => ({
+    title: 'Review Jobs',
+    headerRight: (
+      <Button
+        title="Settings"
+        onPress={() => navigation.navigate('settings')}
+      />
+    )
+  });
   render() {
     return (
       <View>
