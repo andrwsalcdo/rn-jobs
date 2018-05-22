4e1846c2c6da6937d6230326be1c7c594946df5f
andrwsalcdo <asalcedo07@gmail.com>
2018-05-22 16:47:26 +0200

    create a <Slides /> to use in the welcome screen

---
+++ components/Slides.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { View, Text, ScrollView, Dimensions } from 'react-native';
+
+const SCREEN_WIDTH = Dimensions.get('window').width;
+
+class Slides extends Component {
+  renderSlides() {
+    return this.props.data.map(slide => (
+      <View key={slide.text} style={styles.slide}>
+        <Text style={styles.slideText}>{slide.text}</Text>
+      </View>
+    ));
+  }
+
+  render() {
+    return (
+      <ScrollView horizontal pagingEnabled>
+        {this.renderSlides()}
+      </ScrollView>
+    );
+  }
+}
+
+const styles = {
+  slide: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: SCREEN_WIDTH
+  },
+  slideText: {
+    fontSize: 30
+  }
+};
+
+export default Slides;
