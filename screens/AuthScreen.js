8b07761688d813c6afcab2851a5da966a7526bef
andrwsalcdo <asalcedo07@gmail.com>
2018-05-23 20:58:59 +0200

    update AuthScreen to handle facebook login & navigation to map screen once login is successful

--- screens/AuthScreen.js
+++ screens/AuthScreen.js
@@ -1,19 +1,31 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { connect } from 'react-redux';
+import { facebookLogin } from '../redux/actions/authActions';
 
 class AuthScreen extends Component {
+  componentDidMount() {
+    this.props.facebookLogin();
+    this.onAuthComplete(this.props);
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (this.props.token !== prevProps.token) {
+      this.onAuthComplete(this.props);
+    }
+  }
+  onAuthComplete(props) {
+    if (props.token) {
+      this.props.navigation.navigate('map');
+    }
+  }
+
   render() {
-    return (
-      <View>
-        <Text>This is the AuthScreen</Text>
-        <Text>This is the AuthScreen</Text>
-        <Text>This is the AuthScreen</Text>
-        <Text>This is the AuthScreen</Text>
-        <Text>This is the AuthScreen</Text>
-        <Text>This is the AuthScreen</Text>
-      </View>
-    );
+    return <View />;
   }
 }
 
-export default AuthScreen;
+const mapStateToProps = state => ({
+  token: state.auth.token
+});
+
+export default connect(mapStateToProps, { facebookLogin })(AuthScreen);
